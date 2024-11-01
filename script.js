914b702457b86fadbff1f9dc082612aa2eda81ad
FilipSuch <98059283+filsuch@users.noreply.github.com>
2024-11-01 18:48:01 +0100

    verze 1.1, funguje určování osnov a upravovnání pozic

--- script.js
+++ script.js
@@ -4,9 +4,39 @@ let currentNote = '';
 let wrongNotes = new Set(); 
 
 
+let OFFSET = 0
+
+function updateOffset() {
+    const trebleActive = document.getElementById('trebleToggle').checked;
+    const bassActive = document.getElementById('bassToggle').checked;
+    const spacer = document.querySelector('.spacer');
+  //  const trebleClef = document.getElementById('trebleClef');
+    const bassClef = document.getElementById('bassClef');
+
+    if (trebleActive && bassActive) {
+        OFFSET = 0; // Oba klíče 
+        spacer.style.height = '300px';
+        bassClef.style.top = '160px';
+    } else if (bassActive) {
+        OFFSET = -160; // Pouze basový klíč
+        spacer.style.height = '150px';
+        bassClef.style.top = '0px';
+    } else if (trebleActive) {
+        OFFSET = 0; // Pouze houslový klíč
+        spacer.style.height = '150px';
+        bassClef.style.top = '160px';
+    } else {
+        OFFSET = 0; // Žádný klíč
+        spacer.style.height = '0px';
+        bassClef.style.top = '160px';
+    }
+
+    return OFFSET;
+}
 
 
 function generateRandomNote() {
+
 
     const isTrebleActive = document.getElementById('trebleToggle').checked;
     const isBassActive = document.getElementById('bassToggle').checked;
@@ -23,7 +53,7 @@ function generateRandomNote() {
     const noteDisplay = document.getElementById('noteDisplay');
     noteDisplay.innerText = currentNote;
 //  noteDisplay.innerText = '' 
-    noteDisplay.style.top = activeNotes[randomIndex].position;
+    noteDisplay.style.top = (parseInt(activeNotes[randomIndex].position) + OFFSET) + 'px';
     
 //  noteDisplay.style.position = 'relative';
     noteDisplay.style.left = '50%';
@@ -47,8 +77,10 @@ function generateRandomNote() {
     });
     
     
-    wrongNotes.clear();s
-    updateNoteVisibility();
+    wrongNotes.clear();
+    updateOffset();
+ //   updateNoteVisibility();
+    
     
 
 }
@@ -92,27 +124,66 @@ function checkNote(selectedNote) {
     }
 }
 
-document.getElementById('trebleToggle').addEventListener('change', (event) => {
-    document.getElementById('trebleClef').style.display = event.target.checked ? 'block' : 'none';
+document.getElementById('trebleToggle').addEventListener('change', updateDisplay);
+document.getElementById('bassToggle').addEventListener('change', updateDisplay);
+
+function updateDisplay() {
+    const trebleActive = document.getElementById('trebleToggle').checked; 
+    const bassActive = document.getElementById('bassToggle').checked; 
+    noteDisplay.style.display = 'block';
+    // vše on
+    document.getElementById('trebleClef').style.display = 'block'; 
+    document.getElementById('bassClef').style.display = 'block'; 
+
     const trebleLines = document.querySelectorAll('.treble-line');
     trebleLines.forEach(line => {
-        line.style.display = event.target.checked ? 'block' : 'none';
+        line.style.display = 'block'; 
     });
-    updateNoteVisibility();
-});
 
-document.getElementById('bassToggle').addEventListener('change', (event) => {
-    document.getElementById('bassClef').style.display = event.target.checked ? 'block' : 'none';
     const bassLines = document.querySelectorAll('.bass-line');
     bassLines.forEach(line => {
-        line.style.display = event.target.checked ? 'block' : 'none';
+        line.style.display = 'block';
     });
-    updateNoteVisibility();
-});
 
+  
+    if (trebleActive && bassActive) {
+     
+    } else if (trebleActive) {
+     
+        document.getElementById('bassClef').style.display = 'none'; 
+        bassLines.forEach(line => {
+            line.style.display = 'none'; 
+        });
+    } else if (bassActive) {
 
+        document.getElementById('trebleClef').style.display = 'none';
+        const bassLines = document.querySelectorAll('.bass-line');
+        bassLines.forEach(line => {
+            line.style.display = 'none'; 
+        });
+       
+        trebleLines.forEach(line => {
+            line.style.display = 'block'; 
+        });
+        
+    } else {
+   
+        document.getElementById('trebleClef').style.display = 'none'; 
+        document.getElementById('bassClef').style.display = 'none'; 
+        trebleLines.forEach(line => {
+            line.style.display = 'none'; 
+        });
+        bassLines.forEach(line => {
+            line.style.display = 'none'; 
+        });
+        const noteDisplay = document.getElementById('noteDisplay');
+        noteDisplay.style.display = 'none'; // Skryj notu
+    }
 
-
+    
+    updateOffset();
+    generateRandomNote()
+}
 
 // globální 
 window.onload = generateRandomNote;  
--- scripts/notes.js
+++ scripts/notes.js
@@ -1,132 +1,139 @@
+
 const offsets = {
     sharp: { x: 0, y: 0 },
     flat: { x: 0, y: 0 },
     natural: { x: 0, y: 0 },
 };
-const OFFSET = 0;
 
+/*
+
+const OFFSET = -160;
+oba zapnuté = 0
+basový klíč = -160
+pokud bude jen houslový tak = 0
+*/
 export const notes = [
 
-    { name: 'C♯', position: (180 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'C', position: (180 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'C♭', position: (180 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
-    { name: 'D♯', position: (170 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'D', position: (170 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'D♭', position: (170 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'C♯', position: (180) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'C', position: (180) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'C♭', position: (180) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'D♯', position: (170) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'D', position: (170) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'D♭', position: (170) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
-    { name: 'E♯', position: (160 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'E', position: (160 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'E♭', position: (160 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'E♯', position: (160) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'E', position: (160) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'E♭', position: (160) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
-    { name: 'F♯', position: (150 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'F', position: (150 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'F♭', position: (150 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'F♯', position: (150) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'F', position: (150) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'F♭', position: (150) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
-    { name: 'G♯', position: (140 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'G', position: (140 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'G♭', position: (140 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'G♯', position: (140) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'G', position: (140) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'G♭', position: (140) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
-    { name: 'A♯', position: (130 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'A', position: (130 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'A♭', position: (130 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'A♯', position: (130) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'A', position: (130) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'A♭', position: (130) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
-    { name: 'B♯', position: (120 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'B', position: (120 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'B♭', position: (120 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'B♯', position: (120) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'B', position: (120) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'B♭', position: (120) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
     // Bas klič
 
-    { name: 'c♯', position: (110 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'c', position: (110+ OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'c♭', position: (110 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'c♯', position: (110) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'c', position: (110) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'c♭', position: (110) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
-    { name: 'd♯', position: (100 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'd', position: (100 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'd♭', position: (100 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'd♯', position: (100) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'd', position: (100) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'd♭', position: (100) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
-    { name: 'e♯', position: (90 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'e', position: (90 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'e♭', position: (90 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'e♯', position: (90) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'e', position: (90) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'e♭', position: (90) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
-    { name: 'f♯', position: (80 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'f', position: (80 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'f♭', position: (80 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'f♯', position: (80) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'f', position: (80) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'f♭', position: (80) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+ 
+    { name: 'g♯', position: (70) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'g', position: (70) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'g♭', position: (70) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    
+    { name: 'a♯', position: (60) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'a', position: (60) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'a♭', position: (60) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
-    { name: 'g♯', position: (70 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'g', position: (70 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'g♭', position: (70 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'b♯', position: (50) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'b', position: (50) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'b♭', position: (50) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
-    { name: 'a♯', position: (60 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'a', position: (60 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'a♭', position: (60 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
-
-    { name: 'b♯', position: (50 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'b', position: (50 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'b♭', position: (50 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
-
-    { name: 'c1♯', position: (40 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
-    { name: 'c1', position: (40 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
-    { name: 'c1♭', position: (40 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
+    { name: 'c1♯', position: (40) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'bass' },
+    { name: 'c1', position: (40) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'bass' },
+    { name: 'c1♭', position: (40) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'bass' },
 
     // Hous klič
 
-    { name: 'c1♯', position: (0 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'c1', position: (0 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'c1♭', position: (0 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'c1♯', position: (0) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'c1', position: (0) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'c1♭', position: (0) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'd1♯', position: (-10 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'd1', position: (-10 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'd1♭', position: (-10 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'd1♯', position: (-10) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'd1', position: (-10) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'd1♭', position: (-10) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'e1♯', position: (-20 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'e1', position: (-20 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'e1♭', position: (-20 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'e1♯', position: (-20) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'e1', position: (-20) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'e1♭', position: (-20) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'f1♯', position: (-30 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'f1', position: (-30 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'f1♭', position: (-30 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'f1♯', position: (-30) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'f1', position: (-30) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'f1♭', position: (-30) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'g1♯', position: (-40 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'g1', position: (-40 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'g1♭', position: (-40 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'g1♯', position: (-40) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'g1', position: (-40) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'g1♭', position: (-40) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'a1♯', position: (-50 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'a1', position: (-50 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'a1♭', position: (-50 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'a1♯', position: (-50) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'a1', position: (-50) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'a1♭', position: (-50) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'b1♯', position: (-60 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'b1', position: (-60 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'b1♭', position: (-60 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'b1♯', position: (-60) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'b1', position: (-60) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'b1♭', position: (-60) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
     // Druhá oktáva
 
-    { name: 'c2♯', position: (-70 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'c2', position: (-70 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'c2♭', position: (-70 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'c2♯', position: (-70) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'c2', position: (-70) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'c2♭', position: (-70) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'd2♯', position: (-80 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'd2', position: (-80 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'd2♭', position: (-80 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'd2♯', position: (-80) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'd2', position: (-80) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'd2♭', position: (-80) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'e2♯', position: (-90 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'e2', position: (-90 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'e2♭', position: (-90 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'e2♯', position: (-90) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'e2', position: (-90) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'e2♭', position: (-90) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'f2♯', position: (-100 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'f2', position: (-100 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'f2♭', position: (-100 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'f2♯', position: (-100) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'f2', position: (-100) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'f2♭', position: (-100) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'g2♯', position: (-110 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'g2', position: (-110 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'g2♭', position: (-110 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'g2♯', position: (-110) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'g2', position: (-110) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'g2♭', position: (-110) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'a2♯', position: (-120 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'a2', position: (-120 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'a2♭', position: (-120 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'a2♯', position: (-120) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'a2', position: (-120) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'a2♭', position: (-120) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
-    { name: 'b2♯', position: (-130 + OFFSET) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
-    { name: 'b2', position: (-130 + OFFSET) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
-    { name: 'b2♭', position: (-130 + OFFSET) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
+    { name: 'b2♯', position: (-130) + 'px', img: 'image/notex.png', offset: offsets.sharp, clef: 'treble' },
+    { name: 'b2', position: (-130) + 'px', img: 'image/note.png', offset: offsets.natural, clef: 'treble' },
+    { name: 'b2♭', position: (-130) + 'px', img: 'image/noteb.png', offset: offsets.flat, clef: 'treble'},
 
 
 ];
